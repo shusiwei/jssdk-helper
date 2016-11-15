1a33c39e400f71a96182a6a10804cd8d5c18de8d
shusiwei <shusiwei@yeah.net>
2016-11-15 19:01:26 +0800

    removed lodash

--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,41 @@
-'use strict';
-
-exports.__esModule = true;
-
-var _lodash = require('lodash');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-require('whatwg-fetch');
-
-var _weixinJsSdk = require('weixin-js-sdk');
-
-var _weixinJsSdk2 = _interopRequireDefault(_weixinJsSdk);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _newArrowCheck(innerThis, boundThis) { if (innerThis !== boundThis) { throw new TypeError("Cannot instantiate an arrow function"); } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } } /* ==================================================
-                                                                                                                                                           * @ 微信公众平台开发者文档
-                                                                                                                                                           * @ url : http://mp.weixin.qq.com/wiki/11/74ad127cc054f6b80759c40f77ec03db.html
-                                                                                                                                                          
-                                                                                                                                                           * @constructor(*) 微信API
-                                                                                                                                                           * - (xhr : json) 请求票据的API
-                                                                                                                                                           *   - (url : url) 请求的接口API
-                                                                                                                                                           *   - (type : url['POST']) 请求方法
-                                                                                                                                                           *   - (data : url[{}]) 请求接口时的参数
-                                                                                                                                                           *   - (dataType : url [json]) 接口返回的数据格式
-                                                                                                                                                           * - (cfg : json) 分享的配置
-                                                                                                                                                           *   - (title : string[document.title]) 分享的标题
-                                                                                                                                                           *   - (desc : string[document.title]) 分享的描述
-                                                                                                                                                           *   - (link : string[document.title]) 分享的链接地址
-                                                                                                                                                           *   - (imgUrl : string) 分享的图片路径
-                                                                                                                                                           *   - (callback : json) 回调
-                                                                                                                                                           *     - (success : function) 分享成功时的回调
-                                                                                                                                                           *     - (cancel : function) 分享失败时的回调
-                                                                                                                                                           *     -----------------------------------------------
-                                                                                                                                                           *     - (timeline/message/qq/qzone : json) 不同的分享目标不同的回调
-                                                                                                                                                           *       - (success : function) 分享成功时的回调
-                                                                                                                                                           *       - (cancel : function) 分享失败时的回调
-                                                                                                                                                           *       -----------------------------------------------
-                                                                                                                                                           *   - (options : json) 其它配置
-                                                                                                                                                           *     - (menu : boolean) 是否显示更多菜单的按钮
-                                                                                                                                                           *     - (api : array) 需要使用的功能API列表
-                                                                                                                                                           *     - (item : boolean) 显示的功能按钮
-                                                                                                                                                           *       - (base : boolean) 是否显示基础功能按钮
-                                                                                                                                                           *       - (hide : array) 隐藏的功能按钮
-                                                                                                                                                           *       - (show : array) 显示的功能按钮
-                                                                                                                                                           * ================================================== */
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+/* ==================================================
+ * @ 微信公众平台开发者文档
+ * @ url : http://mp.weixin.qq.com/wiki/11/74ad127cc054f6b80759c40f77ec03db.html
+
+ * @constructor(*) 微信API
+ * - (xhr : json) 请求票据的API
+ *   - (url : url) 请求的接口API
+ *   - (type : url['POST']) 请求方法
+ *   - (data : url[{}]) 请求接口时的参数
+ *   - (dataType : url [json]) 接口返回的数据格式
+ * - (cfg : json) 分享的配置
+ *   - (title : string[document.title]) 分享的标题
+ *   - (desc : string[document.title]) 分享的描述
+ *   - (link : string[document.title]) 分享的链接地址
+ *   - (imgUrl : string) 分享的图片路径
+ *   - (callback : json) 回调
+ *     - (success : function) 分享成功时的回调
+ *     - (cancel : function) 分享失败时的回调
+ *     -----------------------------------------------
+ *     - (timeline/message/qq/qzone : json) 不同的分享目标不同的回调
+ *       - (success : function) 分享成功时的回调
+ *       - (cancel : function) 分享失败时的回调
+ *       -----------------------------------------------
+ *   - (options : json) 其它配置
+ *     - (menu : boolean) 是否显示更多菜单的按钮
+ *     - (api : array) 需要使用的功能API列表
+ *     - (item : boolean) 显示的功能按钮
+ *       - (base : boolean) 是否显示基础功能按钮
+ *       - (hide : array) 隐藏的功能按钮
+ *       - (show : array) 显示的功能按钮
+ * ================================================== */
+import _ from 'wizard';
+import 'whatwg-fetch';
+import jssdk from 'weixin-js-sdk';
 
 var JssdkHelper = function () {
   function JssdkHelper(request) {
@@ -68,11 +56,11 @@ var JssdkHelper = function () {
     };
     var imgUrl = config.imgUrl;
 
-    var apiList = _lodash2.default.isArray(options.apiList) ? options.apiList : ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone', 'showOptionMenu', 'hideOptionMenu', 'hideMenuItems', 'showMenuItems', 'hideAllNonBaseMenuItem', 'showAllNonBaseMenuItem'];
-    var hideMenu = _lodash2.default.isBoolean(options.hideMenu) ? options.hideMenu : false;
-    var showBase = _lodash2.default.isBoolean(options.showBase) ? options.showBase : false;
-    var hideItem = _lodash2.default.isArray(options.hideItem) ? options.hideItem : [];
-    var showItem = _lodash2.default.isArray(options.showItem) ? options.showItem : ['menuItem:share:appMessage', 'menuItem:share:timeline', 'menuItem:share:qq', 'menuItem:share:QZone', 'menuItem:favorite'];
+    var apiList = _.isArray(options.apiList) ? options.apiList : ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone', 'showOptionMenu', 'hideOptionMenu', 'hideMenuItems', 'showMenuItems', 'hideAllNonBaseMenuItem', 'showAllNonBaseMenuItem'];
+    var hideMenu = _.isBoolean(options.hideMenu) ? options.hideMenu : false;
+    var showBase = _.isBoolean(options.showBase) ? options.showBase : false;
+    var hideItem = _.isArray(options.hideItem) ? options.hideItem : [];
+    var showItem = _.isArray(options.showItem) ? options.showItem : ['menuItem:share:appMessage', 'menuItem:share:timeline', 'menuItem:share:qq', 'menuItem:share:QZone', 'menuItem:favorite'];
 
     this.share = { title: title, desc: desc, link: link, callback: callback, imgUrl: imgUrl };
     this.config = { apiList: apiList, hideMenu: hideMenu, showBase: showBase, hideItem: hideItem, showItem: showItem };
@@ -94,7 +82,7 @@ var JssdkHelper = function () {
         response.json().then(function (data) {
           _newArrowCheck(this, _this);
 
-          _weixinJsSdk2.default.config({
+          jssdk.config({
             debug: false,
             appId: data.appId,
             timestamp: data.timestamp,
@@ -157,37 +145,37 @@ var JssdkHelper = function () {
     var imgSrc = data ? data.imgUrl || share.imgUrl : share.imgUrl;
     var callback = data ? data.callback || share.callback : share.callback;
 
-    _weixinJsSdk2.default.ready(function () {
+    jssdk.ready(function () {
       _newArrowCheck(this, _this2);
 
       var tempImg = new Image();
       var imgUrl = tempImg.src = imgSrc;
 
-      _weixinJsSdk2.default.onMenuShareAppMessage(_lodash2.default.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl, type: 'link', dataUrl: '' }, this.getCallback(callback, 'message')));
-      _weixinJsSdk2.default.onMenuShareTimeline(_lodash2.default.assign({ title: title, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'timeline')));
-      _weixinJsSdk2.default.onMenuShareQQ(_lodash2.default.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'qq')));
-      _weixinJsSdk2.default.onMenuShareQZone(_lodash2.default.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'qzone')));
+      jssdk.onMenuShareAppMessage(_.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl, type: 'link', dataUrl: '' }, this.getCallback(callback, 'message')));
+      jssdk.onMenuShareTimeline(_.assign({ title: title, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'timeline')));
+      jssdk.onMenuShareQQ(_.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'qq')));
+      jssdk.onMenuShareQZone(_.assign({ title: title, desc: desc, link: link, imgUrl: imgUrl }, this.getCallback(callback, 'qzone')));
 
       if (config.hideMenu) {
-        _weixinJsSdk2.default.showOptionMenu();
+        jssdk.showOptionMenu();
       } else {
-        _weixinJsSdk2.default.hideOptionMenu();
+        jssdk.hideOptionMenu();
       };
 
       if (config.showBase) {
-        _weixinJsSdk2.default.showAllNonBaseMenuItem();
+        jssdk.showAllNonBaseMenuItem();
       } else {
-        _weixinJsSdk2.default.hideAllNonBaseMenuItem();
+        jssdk.hideAllNonBaseMenuItem();
       };
 
       if (config.hideItem.length > 0) {
-        _weixinJsSdk2.default.hideMenuItems({
+        jssdk.hideMenuItems({
           menuList: config.hideItem
         });
       };
 
       if (config.showItem.length > 0) {
-        _weixinJsSdk2.default.showMenuItems({
+        jssdk.showMenuItems({
           menuList: config.showItem
         });
       };
@@ -196,8 +184,8 @@ var JssdkHelper = function () {
 
   JssdkHelper.prototype.getCallback = function getCallback(callback, type) {
     return {
-      success: _lodash2.default.isPlainObject(callback[type]) && 'success' in callback[type] ? callback[type].success : callback.success || this.config.share.callback.success,
-      cancel: _lodash2.default.isPlainObject(callback[type]) && 'cancel' in callback[type] ? callback[type].cancel : callback.cancel || this.config.share.callback.cancel
+      success: _.isPlainObject(callback[type]) && 'success' in callback[type] ? callback[type].success : callback.success || this.config.share.callback.success,
+      cancel: _.isPlainObject(callback[type]) && 'cancel' in callback[type] ? callback[type].cancel : callback.cancel || this.config.share.callback.cancel
     };
   };
 
@@ -206,4 +194,4 @@ var JssdkHelper = function () {
 
 ;
 
-exports.default = JssdkHelper;
+export default JssdkHelper;
